199f76f584094173c7bc1004a3980227174e0760
Vijaya <vijvel@leftbrainpath.com>
2026-02-07 17:06:36 +0530

    added animal.java

---
+++ Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+
+    public String name;
+    public String species;
+
+    public Animal(String name, String species){
+        this.name = name;
+        this.species = species;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
